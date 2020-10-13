59e82a66ef2fdde9c987c507512d543d00786910
SumitBaria <imsumitbaria@gmail.com>
2020-10-13 21:25:24 +0530

    fist commit

--- src/core/Home.js
+++ src/core/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
     const loadAllProducts = () => {
         getProducts()
             .then((data) => {
-                if (data.error){
+                if(data.error){
                     setError(data.error);
                     console.log(error);
                 } else {
